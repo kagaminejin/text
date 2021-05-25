16a8097f5a3fc10f307e6ba6769a8afd41bc2cbf
kagaminejin <78847255+kagaminejin@users.noreply.github.com>
2021-05-25 22:06:08 +0800

    Add files via upload

---
+++ receive.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="txt/html;charset=utf-8">
+	<title>receive</title>
+</head>
+
+<body>
+<?php echo $_GET["id"];?><br>
+<?php echo $_GET["pw"];?>
+
+</body>
+</html>
